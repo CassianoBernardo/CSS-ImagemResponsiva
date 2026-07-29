4398e21b6083ff7e3bb93f2b7a9635fa3812822e
CassianoBernardo <cassianokina@gmail.com>
2026-07-29 13:28:30 +0000

    Feat: Imagem adaptada para telas com resoluções diferentes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ function App() {
 
   return (
     <>
-    <img src="https://picsum.photos/400/300" width="400px"/>
-
+    <picture>
+      <source srcSet="https://picsum.photos/id/5/1200/1200" media="(min-width: 1200px)" />
+      <source srcSet="https://picsum.photos/id/5/1000/1000" media="(min-width: 750px)" />
+      <source srcSet="https://picsum.photos/id/5/750/750" media="(min-width: 500px)" />
+      <img src="https://picsum.photos/id/5/500/500" />
+    </picture>
     </>
   )
 }
